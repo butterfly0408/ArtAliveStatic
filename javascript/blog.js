825925e20d7bb2ce920b805f20d4eb1dac62b441
unknown <vlombard1998@gmail.com>
2024-02-01 13:10:56 +0200

    Blog Update 1Feb24

--- javascript/blog.js
+++ javascript/blog.js
@@ -27,3 +27,78 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Select the DOM-element, so that you can replace it with content
+// let PROJECT_ID = "gem3vv6n";
+// let DATASET = "production";
+// let QUERY = encodeURIComponent('*[_type == "post"] | order(date desc) {title, date, post}');
+
+// // Compose the URL for your project's endpoint and add the query
+// let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+// // fetch the content
+// console.log("fetching");
+// fetch(URL)
+//     .then((res) => res.json())
+//     .then(({ result }) => {
+//         // get the list element, and the first item
+//         let list = document.querySelector("ul");
+//         let firstListItem = document.querySelector("ul li");
+
+//         if (result.length > 0) {
+//             // remove the placeholder content
+//             list.removeChild(firstListItem);
+
+//             result.forEach((post) => {
+//             // create a list element for each post
+//             let listItem = document.createElement("li");
+//             let postTitle = document.createElement("h3");
+//             let postText = document.createElement("p");
+//             let postDate = document.createElement("h6");
+
+//             // add the hero name as the text content
+//             postTitle.textContent = post?.title;
+//             postText.innerHTML = post?.post;
+//             postDate.textContent = post?.date;
+//             listItem.appendChild(postTitle);
+//             listItem.appendChild(postText);
+//             listItem.appendChild(postDate);
+
+//             // add the item to the list
+//             list.appendChild(listItem);
+//         });
+//             let pre = document.querySelector("pre");
+//             // add the raw data to the preformatted element
+//             // pre.textContent = JSON.stringify(result, null, 2);
+//         }
+//     })
+//     .catch((err) => console.error(err));
